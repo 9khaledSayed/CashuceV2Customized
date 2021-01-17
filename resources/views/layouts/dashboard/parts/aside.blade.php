271eac7b7b7a0169b13aa7226b68aa77875c5561
9khaledSayed <ksayed737@gmail.com>
2021-01-17 17:07:48 +0200

    .

--- resources/views/layouts/dashboard/parts/aside.blade.php
+++ resources/views/layouts/dashboard/parts/aside.blade.php
@@ -197,33 +197,8 @@
                             <li class="kt-menu__item " aria-haspopup="true"><a href="{{route('dashboard.allowances.index')}}" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">{{__('Allowances')}}</span></a></li>
                             <li class="kt-menu__item " aria-haspopup="true"><a href="{{route('dashboard.work_shifts.index')}}" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">{{__('Work Shifts')}}</span></a></li>
                             <li class="kt-menu__item " aria-haspopup="true"><a href="{{route('dashboard.vacation_types.index')}}" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">{{__('Vacations Types')}}</span></a></li>
-                            <li class="kt-menu__item  kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="kt-menu__link kt-menu__toggle"><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"></i><span class="kt-menu__link-text">{{__('Departments')}}</span></a>
-                                <div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
-                                    <ul class="kt-menu__subnav">
-                                        <li class="kt-menu__item  kt-menu__item--parent" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">Config</span></span></li>
-
-                                        <li class="kt-menu__item " aria-haspopup="true"><a href="{{route('dashboard.departments.index')}}" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">{{__('AlL Departments')}}</span></a></li>
-
-
-                                        <li class="kt-menu__item " aria-haspopup="true"><a href="{{route('dashboard.departments.create')}}" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">{{__('New Department')}}</span></a></li>
-
-                                    </ul>
-                                </div>
-                            </li>
-
-                            <li class="kt-menu__item  kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="kt-menu__link kt-menu__toggle"><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"></i><span class="kt-menu__link-text">{{__('Sections')}}</span></a>
-                                <div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
-                                    <ul class="kt-menu__subnav">
-                                        <li class="kt-menu__item  kt-menu__item--parent" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">Config</span></span></li>
-
-                                        <li class="kt-menu__item " aria-haspopup="true"><a href="{{route('dashboard.sections.index')}}" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">{{__('AlL Sections')}}</span></a></li>
-
-
-                                        <li class="kt-menu__item " aria-haspopup="true"><a href="{{route('dashboard.sections.create')}}" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">{{__('New Section')}}</span></a></li>
-
-                                    </ul>
-                                </div>
-                            </li>
+                            <li class="kt-menu__item " aria-haspopup="true"><a href="{{route('dashboard.departments.index')}}" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">{{__('Departments')}}</span></a></li>
+                            <li class="kt-menu__item " aria-haspopup="true"><a href="{{route('dashboard.sections.index')}}" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">{{__('Sections')}}</span></a></li>
                         </ul>
                     </div>
                 </li>
